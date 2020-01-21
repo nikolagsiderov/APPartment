38d3e0f2ca7f4204c59c1a4c0e4e2aa5760457cc
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-01-21 23:57:36 +0300

    optimize breadcrumb UI

--- APPartment/Migrations/20200121205134_Initial.Designer.cs
+++ APPartment/Migrations/20200121205134_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APPartment.Migrations
 {
     [DbContext(typeof(DataAccessContext))]
-    [Migration("20191209111735_Initial")]
+    [Migration("20200121205134_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
