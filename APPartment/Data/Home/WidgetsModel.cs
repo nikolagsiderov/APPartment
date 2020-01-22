f0dc678a8895440bdc1e38e17bae7b2c50b9f29f
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-01-22 19:49:19 +0300

    display objects last updates in a fancier way in widgets

---
+++ APPartment/Data/Home/WidgetsModel.cs
@@ -0,0 +1,15 @@
+﻿using APPartment.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APPartment.Data.Home
+{
+    public class WidgetsModel
+    {
+        public List<BaseObject> DisplayObjects { get; set; }
+
+        public List<string> ObjectsLastUpdate { get; set; }
+    }
+}
