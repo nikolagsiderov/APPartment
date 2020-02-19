28828db0a54bd98da96cb7d84c4d98d3f5feeffc
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-02-20 00:07:19 +0300

    add badges in menu for critical items count

--- APPartment/Controllers/HygieneController.cs
+++ APPartment/Controllers/HygieneController.cs
@@ -4,19 +4,40 @@ using APPartment.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using SmartBreadcrumbs.Attributes;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using APPartment.Enums;
 
 namespace APPartment.Controllers
 {
     public class HygieneController : BaseCRUDController<Hygiene>
     {
+        private readonly DataAccessContext _context;
+
         public HygieneController(DataAccessContext context) : base(context)
         {
+            _context = context;
         }
 
         [Breadcrumb("<i class='fas fa-recycle' style='font-size:20px'></i> Hygiene")]
         public override Task<IActionResult> Index(string sortOrder, string searchString)
         {
             return base.Index(sortOrder, searchString);
+        }
+
+        public JsonResult GetHygieneCriticalCount()
+        {
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("HouseId")))
+            {
+                long? currentHouseId = long.Parse(HttpContext.Session.GetString("HouseId"));
+
+                var hygieneCriticalCount = _context.Set<Hygiene>().ToList().Where(x => x.HouseId == currentHouseId && (x.Status == (int)ObjectStatus.Critical ||
+                x.Status == (int)ObjectStatus.High)).Count();
+
+                return Json(hygieneCriticalCount);
+            }
+
+            return Json(0);
         }
 
         public override void PopulateViewData()
--- APPartment/Controllers/InventoryController.cs
+++ APPartment/Controllers/InventoryController.cs
@@ -4,19 +4,40 @@ using APPartment.Controllers.Base;
 using SmartBreadcrumbs.Attributes;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using APPartment.Enums;
 
 namespace APPartment.Controllers
 {
     public class InventoryController : BaseCRUDController<Inventory>
     {
+        private readonly DataAccessContext _context;
+
         public InventoryController(DataAccessContext context) : base(context)
         {
+            _context = context;
         }
 
         [Breadcrumb("<i class='fas fa-tasks' style='font-size:20px'></i> Inventory")]
         public override Task<IActionResult> Index(string sortOrder, string searchString)
         {
             return base.Index(sortOrder, searchString);
+        }
+
+        public JsonResult GetInventoryCriticalCount()
+        {
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("HouseId")))
+            {
+                long? currentHouseId = long.Parse(HttpContext.Session.GetString("HouseId"));
+
+                var hygieneCriticalCount = _context.Set<Inventory>().ToList().Where(x => x.HouseId == currentHouseId && (x.Status == (int)ObjectStatus.Critical ||
+                x.Status == (int)ObjectStatus.High)).Count();
+
+                return Json(hygieneCriticalCount);
+            }
+
+            return Json(0);
         }
 
         public override void PopulateViewData()
