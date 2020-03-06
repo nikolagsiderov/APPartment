101bee67084795df490b65a9175a43fd86661557
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-03-07 00:18:59 +0300

    allow images to upload dynamically

--- APPartment/wwwroot/js/images.custom.js
+++ APPartment/wwwroot/js/images.custom.js
@@ -5,9 +5,13 @@
         acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
         uploadMultiple: true,
         init: function () {
-            this.on("complete", function (data) {
-                //var res = eval('(' + data.xhr.responseText + ')');
-                var res = JSON.parse(data.xhr.responseText);
+            this.on("complete", function (file) {
+                if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+                    $.get(location.href).then(function (page) {
+                        $("#images-main-div").html($(page).find("#images-main-div").html())
+                    })
+                }
+                this.removeFile(file);
             });
         }
     };
