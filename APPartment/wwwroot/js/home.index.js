33ead1e52700627f6630144643bc9b5892b0d4bf
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-03-24 16:51:24 +0200

    home status widget didnt adapt immediatelly after setting

--- APPartment/wwwroot/js/home.index.js
+++ APPartment/wwwroot/js/home.index.js
@@ -24,8 +24,28 @@ $(document).ready(function () {
         url: "Home/GetHomeStatus",
         success: function (data) {
             var dataArr = data.split(";");
+            var status = parseInt(dataArr[0]);
             var username = dataArr[1];
             var details = dataArr[2];
+
+            $('#houseStatusWidget').removeAttr('class');
+
+            if (status === 1) {
+                $('#homeStatus').html('<span class="btn btn-circle btn-success btn-sm"><i class="fas fa-thumbs-up"></i></span>');
+                $('#houseStatusWidget').addClass('card bg-success text-white shadow');
+            } else if (status === 2) {
+                $('#homeStatus').html('<span class="btn btn-circle btn-warning btn-sm"><i class="fas fa-exclamation"></i></span>');
+                $('#houseStatusWidget').addClass('card bg-warning text-white shadow');
+            } else {
+                $('#homeStatus').html('<span class="btn btn-circle btn-danger btn-sm"><i class="fas fa-ban"></i></span>');
+                $('#houseStatusWidget').addClass('card bg-danger text-white shadow');
+            }
+
+            if (details && details != "") {
+                $('#homeStatusDetailsNavItem').html(details);
+            }
+
+            $('#homeStatusUserSetNavItem').html(username);
 
             $('#houseStatusWidgetBody').html(`Home status has been set by ${username}` + `<div class="text-white-50 small">${details}</div>`);
         },
--- APPartment/wwwroot/js/set.home.status.partialview.js
+++ APPartment/wwwroot/js/set.home.status.partialview.js
@@ -11,32 +11,40 @@
             alert(error);
         },
         complete: function (response) {
-            $.ajax({
-                url: "/Home/GetHomeStatus",
-                success: function (data) {
-                    var dataArr = data.split(";");
-                    var status = parseInt(dataArr[0]);
-                    var username = dataArr[1];
-                    var details = dataArr[2];
+        }
+    });
 
-                    if (status === 1) {
-                        $('#homeStatus').html('<span class="btn btn-circle btn-success btn-sm"><i class="fas fa-thumbs-up"></i></span>');
-                    } else if (status === 2) {
-                        $('#homeStatus').html('<span class="btn btn-circle btn-warning btn-sm"><i class="fas fa-exclamation"></i></span>');
-                    } else {
-                        $('#homeStatus').html('<span class="btn btn-circle btn-danger btn-sm"><i class="fas fa-ban"></i></span>');
-                    }
+    $.ajax({
+        url: "/Home/GetHomeStatus",
+        success: function (data) {
+            var dataArr = data.split(";");
+            var status = parseInt(dataArr[0]);
+            var username = dataArr[1];
+            var details = dataArr[2];
 
-                    if (details && details != "") {
-                        $('#homeStatusDetailsNavItem').html(details);
-                    }
+            $('#houseStatusWidget').removeAttr('class');
 
-                    $('#homeStatusUserSetNavItem').html(username);
-                },
-                error: function (req, status, error) {
-                    alert(error);
-                }
-            });
+            if (status === 1) {
+                $('#homeStatus').html('<span class="btn btn-circle btn-success btn-sm"><i class="fas fa-thumbs-up"></i></span>');
+                $('#houseStatusWidget').addClass('card bg-success text-white shadow');
+            } else if (status === 2) {
+                $('#homeStatus').html('<span class="btn btn-circle btn-warning btn-sm"><i class="fas fa-exclamation"></i></span>');
+                $('#houseStatusWidget').addClass('card bg-warning text-white shadow');
+            } else {
+                $('#homeStatus').html('<span class="btn btn-circle btn-danger btn-sm"><i class="fas fa-ban"></i></span>');
+                $('#houseStatusWidget').addClass('card bg-danger text-white shadow');
+            }
+
+            if (details && details != "") {
+                $('#homeStatusDetailsNavItem').html(details);
+            }
+
+            $('#homeStatusUserSetNavItem').html(username);
+
+            $('#houseStatusWidgetBody').html(`Home status has been set by ${username}` + `<div class="text-white-50 small">${details}</div>`);
+        },
+        error: function (req, status, error) {
+            alert(error);
         }
     });
 });
