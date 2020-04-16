37e0d4ada1b00dbd4665c95e4d448b669da9a4af
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-04-16 12:30:44 +0300

    started the implementation of general calendar module

---
+++ APPartment/Controllers/CalendarController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using APPartment.DisplayModels.Calendar;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APPartment.Controllers
+{
+    public class CalendarController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new EventViewModel());
+        }
+
+        public JsonResult GetEvents(DateTime start, DateTime end)
+        {
+            var viewModel = new EventViewModel();
+            var events = new List<EventViewModel>();
+            start = DateTime.Today.AddDays(-14);
+            end = DateTime.Today.AddDays(-11);
+
+            for (var i = 1; i <= 5; i++)
+            {
+                events.Add(new EventViewModel()
+                {
+                    Id = i,
+                    Title = "Event " + i,
+                    Start = start.ToString(),
+                    End = end.ToString(),
+                    AllDay = false
+                });
+
+                start = start.AddDays(7);
+                end = end.AddDays(7);
+            }
+
+
+            return Json(events.ToArray());
+        }
+    }
+}
---
+++ APPartment/DisplayModels/Calendar/EventViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace APPartment.DisplayModels.Calendar
+{
+    public class EventViewModel
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Start { get; set; }
+
+        public string End { get; set; }
+
+        public bool AllDay { get; set; }
+    }
+}
