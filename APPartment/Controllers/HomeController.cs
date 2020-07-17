804628b87a4a91d0807aaab3fdabf8d9455a6d01
Aleksander210 <aleksandar210@abv.bg>
2020-07-17 16:58:13 +0300

    updated chat functionality

--- APPartment/Controllers/HomeController.cs
+++ APPartment/Controllers/HomeController.cs
@@ -213,6 +213,7 @@ namespace APPartment.Controllers
         [HttpPost]
         public async Task<ActionResult> CreateMessage(string username, string messageText)
         {
+            
             var adjustedMessage = string.Join(" <br /> ", messageText.Split('\n').ToList());
             var message = new Message() { Username = username, Text = adjustedMessage, UserId = (long)CurrentUserId, HomeId = (long)CurrentHomeId, CreatedDate = DateTime.Now };
 
--- APPartment/wwwroot/js/chat.js
+++ APPartment/wwwroot/js/chat.js
@@ -2,6 +2,9 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/Home/Index").build();
 
+
+
+
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
@@ -25,22 +28,56 @@ connection.on("ReceiveMessage", function (user, message) {
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+ document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
+
+
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+   
+    
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
 
+
+$(document).ready(function () {
+    $('#sendButton').attr('disabled',true)
+
+    $('#messageInput').on("keyup",function () {
+        var textarea_value = $('#messageInput').val();
+        if (textarea_value!='') {
+            $('#sendButton').attr('disabled', false)
+        }
+        else {
+            $('#sendButton').attr('disabled', true)
+        }
+    });
+   
+});
+
+
+$("#messageInput").keypress(function (e) {
+    var test = $("#messageInput").val()
+    if (e.which == 13 && test.trim() == "") {
+        e.preventDefault();
+    } else if (e.which == 13) {
+        document.getElementById("sendButton").click();
+    }
+});
+
+
+
 $('#messageInput').on("keydown", function (e) {
     if (e.keyCode == 13 && !e.shiftKey) {
         document.getElementById("sendButton").click();
     }
+
+   
 });
