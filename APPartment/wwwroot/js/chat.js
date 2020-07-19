b06025819b14f63d9d5920df6e4436309ed2ba7d
Nikola Siderov <iamnikkysiderov@gmail.com>
2020-07-19 17:26:07 +0300

    forgot this

--- APPartment/wwwroot/js/chat.js
+++ APPartment/wwwroot/js/chat.js
@@ -4,6 +4,13 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/Home/Index").build
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = true;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
 connection.on("ReceiveMessage", function (user, message) {
     var msgArr = message.split(/(?:\r\n|\r|\n)/g);
     var firstMsg = user + ": " + msgArr[0];
